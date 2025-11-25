7a323f0e8f5c4ffbe9e081c0f607ee6854c32e01
Sergei Stokoyasov <temrevser4@gmail.com>
2025-11-26 00:04:32 +0500

    Create ScriptableObject: CellPaletteSettigns

---
+++ Assets/Scripts/Settings/CellPaletteSettings.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Checkers.Settings
+{
+
+    [CreateAssetMenu(fileName = "New CellPaletteSettings", menuName = "Settings/CellPaletteSettings", order = 52)]
+    public class CellPaletteSettings : ScriptableObject
+    {
+        [field: SerializeField, Space(20f)]
+        [field: Tooltip("Клетка под выбранным юнитом")]
+        public Material SelectCell { get; private set; }
+
+        [field: SerializeField]
+        [field: Tooltip("Клетка доступная для передвижения")]
+        public Material MoveCell { get; private set; }
+
+        [field: SerializeField]
+        [field: Tooltip("Клетка доступная для атаки")]
+        public Material AttackCell { get; private set; }
+
+        [field: SerializeField]
+        [field: Tooltip("Клетка доступная для передвижения и атаки")]
+        public Material MoveAndAttackCell { get; private set; }
+    }
+}
+
